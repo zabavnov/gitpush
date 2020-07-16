5662c6620265a33fb9f016b6427231c5b53672d2
Cleon Diedre <cdiedre@ox-m1.msk.21-school.ru>
2020-07-17 00:37:50 +0300

    v1.0

---
+++ gitpush.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [[ $* != *--* ]]
+then
+echo "Use flags:
+--add
+--commit
+--push
+--tree"
+fi
+if [[ "$1" = "--add" ]]
+then
+echo $'.DS_Store\nmain.c\na.out\n.gitignore\ngitpush.sh' > .gitignore
+git init
+git add .
+git status
+read -p "Enter git repo link: " gitlink
+git remote add origin $gitlink
+fi
+if [[ "$1" = "--commit" ]]
+then
+read -p "Enter the message for the new commit: " message
+git commit -m "$message"
+git log
+fi
+if [[ "$1" = "--push" ]]
+then
+git push origin master
+fi
+if [[ "$1" = "--tree" ]]
+then
+git ls-tree -r master
+fi
+git status
